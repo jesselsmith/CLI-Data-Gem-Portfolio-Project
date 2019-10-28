8fde4e1652e51a070ead69d6cf9245dc1f34343d
Jesse <jesse.lee.smith@gmail.com>
2019-10-28 15:09:58 -0700

    Defined much of card class

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,50 @@
-class Card
+# frozen_string_literal: true
 
+class Card
+  attr_accessor :name, :mana_cost, :card_url, :daily_change, :weekly_change,
+                :highest_price, :lowest_price, :sets
+  attr_reader :decks
+
+  @@all = []
+
+  def initialize(name:, mana_cost:, card_url:)
+    @name = name
+    @mana_cost = mana_cost
+    @card_url = card_url
+  end
+
+  def self.create(name:, mana_cost:, card_url:)
+    self.new(name: name, mana_cost: mana_cost, card_url: card_url)
+    @@all << self
+  end
+
+  # takes in a name string and converts it to a symbol
+  def self.symbolize(name_string)
+    # replaces all the spaces with underscores, downcases before converting
+    name_string.gsub(/\s+/, '_').downcase.to_sym
+  end
+
+  def add_deck(deck_object)
+    name_symbol = symbolize(deck_object.name)
+    if @decks.is_a? Hash
+      @decks[name_symbol] = deck_object unless @decks.key?(name_symbol)
+    else
+      @decks = {
+        name_symbol => deck_object
+      }
+    end
+    deck_object.add_card(self) unless deck_object.has_card? self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_set(set_name_string)
+    if @sets.is_a? Array
+      @sets << set_name_string unless @sets.include? set_name_string
+    else
+      @sets = [set_name_string]
+    end
+  end
 end
--- spec/01_oo_spec.rb
+++ spec/01_oo_spec.rb
@@ -1,3 +1,23 @@
 require_relative 'spec_helper'
 
-Scraper.test
+describe "Card" do
+  let(:card) { Card.new("Primeval Titan", "4GG", "price/Magic+2012/Primeval+Titan") }
+  describe "#initialize" do
+    it 'accepts a name, mana_cost, and card_url' do
+      new_card = Card.new(name: "Oko, Thief of Crowns", mana_cost: "1UG", card_url: "price/Throne+of+Eldraine/Oko+Thief+of+Crowns")
+      expect(new_card.name).to eq("Oko, Thief of Crowns")
+      expect(new_card.mana_cost).to eq("1UG")
+      expect(new_card.card_url).to eq("price/Throne+of+Eldraine/Oko+Thief+of+Crowns")
+    end
+  end
+end
+
+
+describe "Deck" do
+  describe "#initialize" do
+    it 'accepts a name, colors, featured_cards, meta_percent, and deck_url' do
+      deck = Deck.new("UG Walkers", "UG", "")
+      expect 
+    end
+  end
+end
