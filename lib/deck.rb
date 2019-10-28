b2e04cef80bc1779cc4cc4524af0ada1572a1376
Jesse <jesse.lee.smith@gmail.com>
2019-10-28 15:48:29 -0700

    Defined Deck#initialize and Deck.create

--- lib/deck.rb
+++ lib/deck.rb
@@ -5,10 +5,26 @@ class Deck
                 :paper_price, :deck_url
   attr_reader :cards
 
+  @@all =  []
+
   def initialize(name:, featured_cards:, colors:, meta_percent:, online_price:, paper_price:, deck_url:)
     @name = name
-    @featured_cards = fee
-
+    @featured_cards = featured_cards
+    @colors = colors
+    @meta_percent = meta_percent
+    @online_price = online_price
+    @paper_price = paper_price
+    @deck_url = deck_url
   end
+
+  def self.create(name:, featured_cards:, colors:, meta_percent:, online_price:, paper_price:, deck_url:)
+    self.new(name: name, featured_cards: featured_cards, colors: colors, meta_percent: meta_percent, online_price: online_price, paper_price: paper_price, deck_url: deck_url)
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 
 end
