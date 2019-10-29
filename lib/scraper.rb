bad1fbc1f57bb9731b2223f68bdf51345fb90c79
Jesse <jesse.lee.smith@gmail.com>
2019-10-28 19:11:17 -0700

    Started working on scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,18 @@
+require 'open-uri'
+require 'nokogiri'
+
 class Scraper
+  BASE_URL = "https://www.mtggoldfish.com/"
+
+  def scrape_top_10_decks(url_add_on) 
+    doc = Nokogiri::HTML(URI.open(BASE_URL + url_add_on))
+
+    decks = doc.css(".archetype-tile")
+
+    names = decks.css(".deck-price-paper a").text
+
+  end
+
 
   
 end
